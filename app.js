a5a44ed3471c2d1914fccc02a6f1d6f0d7be3957
Huang <837317210@qq.com>
2017-07-17 18:01:15 +0800

    add

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var path = require('path');
+var mongoose = require('mongoose');
+var Movie = require('./models/movie');
 var port = process.env.PORT || 3000;
 var app = express();
 
+mongoose.connect('mongodb://localhost/imooc')
 
 app.set('views', './views/pages');//视图的默认目录
 
@@ -21,51 +24,54 @@ console.log('imooc start on port ' + port);
 
 // index page
 app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'imooc 首页',
-    movies: [{
-      title: '机械战警',
-      _id: 1,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    },{
-      title: '机械战警',
-      _id: 2,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    },{
-      title: '机械战警',
-      _id: 3,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    },{
-      title: '机械战警',
-      _id: 4,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    },{
-      title: '机械战警',
-      _id: 5,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    },{
-      title: '机械战警',
-      _id: 6,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-    }]
-  });
+  Movie.fetch(function(err, movies){
+    if(err){
+      console.log(err)
+    }
+    res.render('index', {
+      title: 'imooc 首页',
+       movies: movies
+    });
+  }) 
+  
+  //{
+    //   title: '机械战警',
+    //   _id: 1,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // },{
+    //   title: '机械战警',
+    //   _id: 2,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // },{
+    //   title: '机械战警',
+    //   _id: 3,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // },{
+    //   title: '机械战警',
+    //   _id: 4,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // },{
+    //   title: '机械战警',
+    //   _id: 5,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // },{
+    //   title: '机械战警',
+    //   _id: 6,
+    //   poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+    // }
 });
 
 // detail page
 app.get('/movie/:id', (req, res) => {
-  res.render('detail', {
-    title: 'imooc 详情页',
-    movie: {
-      doctor: '何塞趴地利亚',
-      country: '美国',
-      title: '机械战警',
-      year: 2014,
-      poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-      language: '英语',
-      flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-      summary: '这是一部科幻片'
-    }
-  });
+  var id = req.params.id;
+
+  Movie.findById(id, function(err, movie) {
+    res.render('detail', {
+      title: 'imooc ' + movie.title,
+      movie: movie
+    });
+  })
+  
 });
 
 // list page
@@ -100,3 +106,19 @@ app.get('/admin/movie', (req, res) => {
     }
   });
 });
+
+
+// admin post movie
+app.post('/admin/movie/new', function(req, res){
+  var id = req.body.movie._id;
+  var movieObj = req.body.movie;
+  var _movie;
+  if(id !== 'undefined'){
+    Movie.findById(id, function(err, movie){
+      if(err){
+        console.log(err)
+      }
+      _movie = 
+    })
+  }
+})
