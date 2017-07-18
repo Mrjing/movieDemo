8d987ad2ae3eb20f1fcf2d55ff49219f32e69466
Huang <837317210@qq.com>
2017-07-18 16:37:29 +0800

    add

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var serveStatic = require('serve-static');
 var path = require('path');
 var mongoose = require('mongoose');
+var _ = require('underscore');
 var Movie = require('./models/movie');
 var port = process.env.PORT || 3000;
 var app = express();
@@ -63,38 +64,48 @@ app.get('/', (req, res) => {
 
 // detail page
 app.get('/movie/:id', (req, res) => {
+
   var id = req.params.id;
 
   Movie.findById(id, function(err, movie) {
+    console.log("movie详情:", movie);
     res.render('detail', {
       title: 'imooc ' + movie.title,
       movie: movie
     });
-  })
+  });
   
 });
 
-// list page
+// list pages
 app.get('/admin/list', (req, res) => {
-  res.render('list', {
-    title: 'imooc 列表页',
-    movies: [{
-      title: '机械战警',
-      _id: 1,
-      doctor: '何塞趴地利亚',
-      country: '美国',
-      year: 2014,
-      language: '英语',
-      flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
-    }]
-  });
+  Movie.fetch(function(err, movies){
+    if(err){
+      console.log(err)
+    }
+    res.render('list', {
+      title: 'imooc 列表页',
+      movies: movies 
+      // [{
+      //   title: '机械战警',
+      //   _id: 1,
+      //   doctor: '何塞趴地利亚',
+      //   country: '美国',
+      //   year: 2014,
+      //   language: '英语',
+      //   flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
+      // }]
+    });
+  })
 });
 
-// list page
+//
 app.get('/admin/movie', (req, res) => {
+  
+
   res.render('admin', {
     title: 'imooc 后台录入页',
-    movie: {
+    movie:  {
       title: '',
       doctor: '',
       country: '',
@@ -108,17 +119,55 @@ app.get('/admin/movie', (req, res) => {
 });
 
 
+//admin update movie
+app.get('/admin/update/:id', function(req,res){
+  var id = req.params.id;
+  if(id){
+    Movie.findById(id, function(err, movie){
+      res.render('admin', {
+        title: 'imooc后台更新页',
+        movie: movie
+      })
+    })
+  }
+})
+
 // admin post movie
 app.post('/admin/movie/new', function(req, res){
   var id = req.body.movie._id;
   var movieObj = req.body.movie;
   var _movie;
   if(id !== 'undefined'){
+    console.log("已存在的");
     Movie.findById(id, function(err, movie){
       if(err){
         console.log(err)
       }
-      _movie = 
+      _movie = _.extend(movie, movieObj);
+      _movie.save(function(err, movie) {
+        if(err){
+          console.log(err);
+        }
+        res.redirect('/movie/' + movie._id);
+      })
+    })
+  }else{
+    console.log("新生成的");
+    _movie = new Movie({
+      doctor: movieObj.doctor,
+      title: movieObj.title,
+      country: movieObj.country,
+      language: movieObj.language,
+      year: movieObj.year,
+      poster: movieObj.poster,
+      summary: movieObj.summary,
+      flash: movieObj.flash,
+    });
+    _movie.save(function(err, movie) {
+      if(err){
+        console.log(err);
+      }
+      res.redirect('/movie/' + movie._id);
     })
   }
 })
---
+++ mongoose_study.md
@@ -0,0 +1,8 @@
+---
+title: mongoose学习
+date: 2017-07-17 15:05:04
+tags: 
+- mongoose
+categories: [mongoose]
+---
+## Schema-模式定义 ##
